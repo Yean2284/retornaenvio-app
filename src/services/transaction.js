26d27e19734974b6cf49bb0a343638b0d65d187f
Yean2284 <YCARLOSBV22@GMAIL.COM>
2026-09-05 18:03:56 -0400

    Create transaction service for transaction history
    Added a service to retrieve transaction history.

---
+++ src/services/transaction.js
@@ -0,0 +1,7 @@
+// Servicio de transacciones para Retorna
+export const getTransactionHistory = async () => {
+  return [
+    { id: 'TXN-9841', date: '2026-09-01', amountBrl: 150, recipient: 'Angela Josefina Santoyo Moya', status: 'Completado' },
+    { id: 'TXN-9520', date: '2026-08-25', amountBrl: 80, recipient: 'Leibys Gonzalez', status: 'Completado' }
+  ];
+};
