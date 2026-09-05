0729e58937a2540362dbee7c2ef1b0b24bc1f6c6
Yean2284 <YCARLOSBV22@GMAIL.COM>
2026-09-05 18:07:41 -0400

    Create AppNavigator component
    Add initial AppNavigator component for navigation setup.

---
+++ src/navigation/AppNavigator.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AppNavigator({ currentScreen, onNavigate }) {
+  // Configuración de rutas centralizadas si se requiere expandir React Navigation
+  return null;
+}
+
